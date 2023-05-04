8838d99dee87d329d70dfdef81ee14144de3cb1b
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2023-05-04 15:24:19 -0500

    chore: setup completed

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,6 +1,48 @@
-const { func } = require('../exercise');
+const { challengeOne } = require("../exercise");
 
-test('Testing the function exercise', () => { 
-    expect(func()).toBeDefined();
-    
- })
+test("Testing the function exercise", () => {
+  expect(challengeOne([])).toBeDefined();
+  expect(challengeOne(["a", "s"])).toEqual({
+    a: ["a", "and"],
+    s: ["some", "she", "said", "So"],
+  });
+  expect(challengeOne(["b", "t", "s"])).toEqual({
+    b: [
+      "Betty",
+      "Botter",
+      "butter",
+      "But",
+      "butters",
+      "bitter",
+      "bake",
+      "batter",
+      "bit",
+      "better",
+      "bought",
+      "baked",
+      "Botterbought",
+    ],
+    t: ["this", "that", "than", "the", "twas"],
+    s: ["some", "she", "said", "So"],
+  });
+  expect(challengeOne(["i", "b", "a", "t"])).toEqual({
+    i: ["If", "I", "it", "in"],
+    b: [
+      "Betty",
+      "Botter",
+      "butter",
+      "But",
+      "butters",
+      "bitter",
+      "bake",
+      "batter",
+      "bit",
+      "better",
+      "bought",
+      "baked",
+      "Botterbought",
+    ],
+    a: ["a", "and"],
+    t: ["this", "that", "than", "the", "twas"],
+  });
+});
--- exercise.js
+++ exercise.js
@@ -1,14 +1,39 @@
-/** DO NOT CHANGE THE FUNCTION NAME **/
-const func = (c) => {
-    /* Only make changes below this comment */
-    
-    
-    /* Only make changes below this comment */
+/*
+Given the below paragraph create a function that returns a list
+of unique words that starts with the same letters provided with an array of strings.
+ex challengeOne(["a", "s", "p"])
+
+
+Betty Botter had some butter, But, she said, this butters bitter. 
+If I bake this bitter butter, it would make my batter bitter.
+But a bit of better butterthat would make my batter better.
+So she bought a bit of butter, better than her bitter butter,
+and she baked it in her batter, and the batter was not bitter. 
+So twas better Betty Botterbought a bit of better butter.
+
+interface Answer {
+    letter: string[]
+}
+ex. {
+    a: ["a"],
+    s: ["So", "She"]
 }
 
+*/
 
+// DO NOT CHANGE THE TEXT
+const text = `Betty Botter had some butter, But, she said, this butters bitter. 
+If I bake this bitter butter, it would make my batter bitter. 
+But a bit of better butter that would make my batter better. 
+So she bought a bit of butter, better than her bitter butter, 
+and she baked it in her batter, and the batter was not bitter. 
+So twas better Betty Botterbought a bit of better butter.`;
 
-
+/** DO NOT CHANGE THE FUNCTION NAME **/
+const challengeOne = (letters) => {
+  /* Only make changes below this comment */
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
-module.exports.func = func;
+module.exports.challengeOne = challengeOne;
