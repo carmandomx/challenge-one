fb8a633a9b513ef59f273f29e7f0b9f24534cde9
Alexander Suarez <alexander.suarez@itksquare.edu.mx>
2023-05-05 17:48:14 -0600

    Exercise Done, with comments

--- exercise.js
+++ exercise.js
@@ -32,17 +32,22 @@ So twas better Betty Botterbought a bit of better butter.`;
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const challengeOne = (letters) => {
   /* Only make changes below this comment */
-  const words = text.replace(/[.,]/g, "").split(/\s+/);
+  const words = text.replace(/[.,]/g, "").split(/\s+/); //Creation of an array that will contain every word in the text, using a regular expresion to remove punctuation marks. And then and then splits the text by whitespace
 
+  ////This condition checks if the first occurrence of the current word in the words array is equal to the current index.
+  // If they are equal, it means this is the first occurrence of the word in the array, and the function returns true.
+  // If not, it means the word has already appeared earlier in the array, so it's a duplicate, and the function returns false.
   const uniqueWords = words.filter(
     (word, index, self) => self.indexOf(word) === index
   );
-  const answer = {};
+  const answer = {}; //Create an object that will store the answers
 
   letters.forEach((letter) => {
+    //For each letter passed as parameter
     answer[letter] = uniqueWords.filter(
       (word) => word[0].toLowerCase() === letter.toLowerCase()
-    );
+    ); //Check the uniqueWords array and if the first letter in lowercase of a word is equal to the letter passed as parameter in lowercase
+    //If it is equal, the answer object is filled  with arrays of unique words that start with the provided letters.
   });
 
   return answer;
