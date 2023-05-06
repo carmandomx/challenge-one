1115d51f8dff4d212bf46e124e891e88be2d6b08
SergioEspadas-Ksquare <Sergio.Espadas@itksquare.edu.mx>
2023-05-05 19:34:17 -0600

    comments added

--- exercise.js
+++ exercise.js
@@ -32,22 +32,28 @@ So twas better Betty Botterbought a bit of better butter.`;
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const challengeOne = (letters) => {
   /* Only make changes below this comment */
-
+  // Split the input text into words using non-word characters as delimiters
   let splittedText = text.split(/\W+/);
+  // Create an empty object to store words categorized by their starting letter
   let resultingWords = {};
+  // Create an array of letter options for each given letter, including upper and lowercase
   const options = letters.map((letter) => [
     letter.toUpperCase(),
     letter.toLowerCase(),
   ]);
 
+  // Iterate through the array of letter options
   options.forEach((option, index) => {
-    // Initialize an empty array for the letter if it doesn't exist
+    // Initialize an empty array for the letter if it doesn't exist in the resultingWords object
     if (!resultingWords[letters[index]]) {
       resultingWords[letters[index]] = [];
     }
 
+    // Iterate through each word in the splittedText array
     splittedText.forEach((word) => {
+      // Check if the word starts with either the uppercase or lowercase version of the current letter
       if (word.startsWith(option[0]) || word.startsWith(option[1])) {
+        // If the word is not already in the array for the current letter, add it
         if (!resultingWords[letters[index]].includes(word)) {
           resultingWords[letters[index]].push(word);
         }
